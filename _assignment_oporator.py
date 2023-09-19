8d0b6afd526c671b939fc7f3cb40bb73e6cf5cba
MD. Jahid Hassan <33800850+Jahid-SWE@users.noreply.github.com>
2023-09-19 18:43:13 +0600

    Operators clear example

---
+++ _assignment_oporator.py
@@ -0,0 +1,27 @@
+#Assignment opertor
+
+a = 20
+a +=20
+print(a)
+
+a -= 5
+print(a)
+
+a *= 2
+print(a)
+
+a /= 3
+print(a)
+
+a %=3
+print(a)
+
+a = 4
+a //= 3
+print(a)
+
+
+a =8 
+a**=3
+print(a)
+
