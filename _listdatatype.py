eb2fb27abb9b3c0a5c7cbe0c7bb46c7a8fb17edf
MD. Jahid Hassan <33800850+Jahid-SWE@users.noreply.github.com>
2023-09-19 09:18:28 +0600

    Oporators concept clear in here

---
+++ _listdatatype.py
@@ -0,0 +1,19 @@
+# list 
+lst = ['jahid','hassan','polash','babu']
+print(lst)
+print(type(lst))
+
+# tuple
+tup = ('cat','dog','parrot','tiger')
+#tup[1]='jubel'
+print(tup)
+print(type(tup))
+
+
+# range
+
+ran = range(10)
+for i in ran:
+    print(i, end=' ')
+
+
---
+++ _operator.py
@@ -0,0 +1,36 @@
+# Adition
+a = 20
+b = 30
+print(a+b)
+
+# Subtraction
+a = 20 
+b = 9
+print(a-b)
+
+# Multiplication
+a = 50 
+b = 20 
+print(a*b)
+
+# Division
+a = 25 
+b = 5 
+print(a/b) 
+
+# Modulus
+a =80 
+b = 7 
+print(a%b)
+
+# Exponentiation
+a = 5
+b = 3 
+print(a**b) # 5*5*5=25
+
+
+# Float division
+
+a = 30 
+b = 4 
+print(a//b) # 30 /4 = 7.5 = 7 also contains integer values
