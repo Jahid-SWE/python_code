d159e741dc0f74cd353a5218db7d78575b14324d
MD. Jahid Hassan <33800850+Jahid-SWE@users.noreply.github.com>
2024-12-20 21:23:38 +0600

    variable function type and list dictionary tuple set done today

--- _variable.py
+++ _variable.py
@@ -1,4 +1,22 @@
-name = 'Jahid Hassan'
-number = 102
-isk = True
-print(number)
+name = 'Jahid'
+number= 150
+number_one = 10.5
+number_two = 1+5j
+is_student = False
+list = [1,25,8,4,5,27,10]
+tuple= ('jam', 'karim', 'arif', 'nizam')
+set = {'kamal', 'korim', 10, 90}
+dict ={
+    'student': "julfiqar",
+    'class': 9,
+    'roll':1
+}
+print(type(name))
+print(type(number))
+print(type(number_one))
+print(type(number_two))
+print(type(is_student))
+print(type(list))
+print(type(tuple))
+print(type(set))
+print(list[:2])
