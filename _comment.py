161ecc28ec95ddfea4419ec7ddc180fa806479e6
MD. Jahid Hassan <33800850+Jahid-SWE@users.noreply.github.com>
2024-12-20 17:15:43 +0600

    Comment in python

--- _comment.py
+++ _comment.py
@@ -3,3 +3,5 @@
 '''This is a multi line comments
 I didn't like single like single
 line commnet I like multiline comment '''
+
+print("Here two type of comment in python first is single line comment using (#) and another multi  line comment using (''' ''')")
